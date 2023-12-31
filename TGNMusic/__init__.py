89dfbfd72aeb2b6de6f8e79225b74af3be3d6279
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-12-31 21:17:50 +0530

    Create __init__.py

---
+++ TGNMusic/__init__.py
@@ -0,0 +1,26 @@
+from TGNMusic.core.bot import TGN
+from TGNMusic.core.dir import dirr
+from TGNMusic.core.git import git
+from TGNMusic.core.userbot import Userbot
+from TGNMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = TGN()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
