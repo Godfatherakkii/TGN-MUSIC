e0489cab48aac2276a168f17175843d0b0d98d64
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2024-01-01 18:53:33 +0530

    Update __main__.py

--- TGNMusic/__main__.py
+++ TGNMusic/__main__.py
@@ -1,18 +1,16 @@
 import asyncio
 import importlib
-import sys
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
 from TGNMusic import LOGGER, app, userbot
 from TGNMusic.core.call import TGN
+from TGNMusic.misc import sudo
 from TGNMusic.plugins import ALL_MODULES
 from TGNMusic.utils.database import get_banned_users, get_gbanned
-
-loop = asyncio.get_event_loop()
+from config import BANNED_USERS
 
 
 async def init():
@@ -23,17 +21,8 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("TGNMusic").error(
-            "No Assistant Clients Vars Defined!.. Exiting Process."
-        )
-        return
-    if (
-        not config.SPOTIFY_CLIENT_ID
-        and not config.SPOTIFY_CLIENT_SECRET
-    ):
-        LOGGER("TGNMusic").warning(
-            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
-        )
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -43,30 +32,30 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
+    await sudo()
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("TGNMusic.plugins" + all_module)
-    LOGGER("TGNMusic.plugins").info(
-        "Successfully Imported Modules "
-    )
+    LOGGER("TGNMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await TGN.start()
     try:
-        await TGN.stream_call(
-            "https://graph.org/file/ec8a35dd5f1ef90947167.mp4"
-        )
+        await TGN.stream_call("https://graph.org/file/ec8a35dd5f1ef90947167.mp4")
     except NoActiveGroupCall:
         LOGGER("TGNMusic").error(
-            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
-        sys.exit()
+        exit()
     except:
         pass
     await TGN.decorators()
-    LOGGER("TGNMusic").info("TGN Music Bot Started Successfully")
+    LOGGER("TGNMusic").info(
+        "TGN Music Bot Started Successfully"
+    )
     await idle()
+    await app.stop()
+    LOGGER("TGNMusic").info("Stopping TGN Music Bot...")
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
-    LOGGER("TGNMusic").info("Stopping TGN Music Bot! GoodBye")
+    asyncio.get_event_loop().run_until_complete(init())
